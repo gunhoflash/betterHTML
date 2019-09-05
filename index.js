503a6c3c322d03c9e4baa84a99493f93ab36279b
Geonho Kim <gunhoflash@gmail.com>
2019-09-05 19:42:51 +0900

    Load file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 /*
 	ARGS
 		type: object
@@ -8,25 +10,16 @@
 		[4]: OPTIONS
 
 */
-let ARGS = process.argv;
+let ARGS;
 let INPUT_FILE;
 let OUTPUT_FILE;
 let OPTIONS;
 
-// Check argument
-if (ARGS.length < 4) {
-	log('Command Format: node betterHTML {input_file} {output_file} {options}');
-} else {
-	INPUT_FILE = ARGS[2];
-	OUTPUT_FILE = ARGS[3];
-	OPTIONS = ARGS[4];
-}
-
-console.log('Hello, world!');
+let FILE = null;
 
 /*
 
-	functions
+	common functions
 
 */
 
@@ -34,3 +27,55 @@ console.log('Hello, world!');
 log = obj => {
 	console.log(obj);
 };
+
+/*
+
+	prework functions
+
+*/
+
+// get arguments
+getArgs = () => {
+	ARGS = process.argv;
+	if (ARGS.length < 4) {
+		log('Command Format: node betterHTML {input_file} {output_file} {options}');
+	} else {
+		INPUT_FILE  = ARGS[2];
+		OUTPUT_FILE = ARGS[3];
+		OPTIONS     = ARGS[4];
+	}	
+};
+
+// load INPUT_FILE
+loadFile = () => {
+	try {
+		// read the file
+		FILE = fs.readFileSync(INPUT_FILE, 'utf8');
+	}
+	catch (e) {
+		// file not found
+		FILE = null;
+		log(`No such file:'${INPUT_FILE}'`);
+		return;
+	}
+	log(FILE);
+};
+
+/*
+
+run
+
+*/
+
+// start
+log('[Better HTML]');
+
+// get arguments
+getArgs();
+
+// load the file
+loadFile();
+
+// TODO: remove comment
+// TODO: structure up & check tag-error
+// TODO: check web standard compliance
